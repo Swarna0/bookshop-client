bd34ad2d020e1dcc2490571d02e23e518affe60f
Swarna0 <swarnaranidas1@gmail.com>
2021-04-08 23:04:54 +0600

    first commit

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI8Bqi6SOaMHd8RejZnKzWiVXpN1Xawvc",
+    authDomain: "book-shop01.firebaseapp.com",
+    projectId: "book-shop01",
+    storageBucket: "book-shop01.appspot.com",
+    messagingSenderId: "55671978585",
+    appId: "1:55671978585:web:d84c835984834c3d43cf48"
+  };
+
+
+
+export default firebaseConfig;
---
+++ src/components/HomePage/Homepage.js
@@ -0,0 +1,33 @@
+import React, {useContext, useEffect} from 'react';
+import Search from "../Search/Search";
+import CardComponent from "../CardComponent/CardComponent";
+import {Col, Row} from "react-bootstrap";
+import {BookContext} from "../../App";
+
+const Homepage = () => {
+
+    const [contextBooks, setContextBooks] = useContext(BookContext)
+
+    useEffect(() => {
+        fetch('https://sleepy-fjord-78299.herokuapp.com/api/books')
+        //fetch('https://sleepy-fjord-78299.herokuapp.com/api/books')
+
+            .then(res => res.json())
+            .then(data => setContextBooks(data.books))
+            .catch(err => console.log(err))
+    }, [setContextBooks])
+
+    return (
+        <div className="container">
+            <Search/>
+            <Row className="m-auto">
+                {
+                    contextBooks.map(book => <Col> <CardComponent data={book} key={book._id}/> </Col>)
+                }
+            </Row>
+
+        </div>
+    );
+};
+
+export default Homepage;
